e77ffc11c2b335061944e9d04d6b48ebff443619
twizzworld <132231765+twizzworld@users.noreply.github.com>
2023-08-05 19:52:28 -0500

    added components to app.js to set default camera

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./style.css";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 function CarShow() {
-  return null;
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color={"red"} />
+      </mesh>
+    </>
+  );
 }
 
 function App() {
