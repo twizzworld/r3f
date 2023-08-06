e92f4dbaca7f25f7e149eb631c5ed94833c0f48c
twizzworld <132231765+twizzworld@users.noreply.github.com>
2023-08-05 19:45:06 -0500

    changed app js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import "./style.css";
+
+function CarShow() {
+  return null;
+}
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
+  )
 }
 
 export default App;
